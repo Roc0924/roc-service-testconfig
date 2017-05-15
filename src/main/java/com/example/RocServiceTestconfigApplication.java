3583faeafd61bb8eafcad197c2989a75bea2009a
wangzhenpeng <wangzhenpeng@gomeplus.com>
2017-05-15 14:22:31 +0800

    test

--- src/main/java/com/example/RocServiceTestconfigApplication.java
+++ src/main/java/com/example/RocServiceTestconfigApplication.java
@@ -1,17 +1,22 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@RefreshScope
 public class RocServiceTestconfigApplication {
+	@Value("${environment}")
+	String appName;
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World!";
+		return appName;
 	}
 
 	public static void main(String[] args) {
